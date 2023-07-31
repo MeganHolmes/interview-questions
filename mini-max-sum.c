8e48c7a97c328b77cbdb2d58d70f384c1bf40121
Megan Holmes <meganholmes100@gmail.com>
2023-07-31 13:41:46 -0700

    add mini-max sum problem

---
+++ mini-max-sum.c
@@ -0,0 +1,27 @@
+// From: https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+void miniMaxSum(int arr_count, int* arr) {
+    int lowestIdx = 0;
+    int highestIdx = 0;
+    unsigned long sum = arr[0];
+
+    for (int idx = 1; idx < arr_count; idx++)
+    {
+        sum += arr[idx];
+
+        if (arr[highestIdx] < arr[idx])
+        {
+            highestIdx = idx;
+        }
+
+        if (arr[lowestIdx] > arr[idx])
+        {
+            lowestIdx = idx;
+        }
+    }
+
+    unsigned long lowestPossible = sum - (unsigned long)arr[highestIdx];
+    unsigned long highestPossible = sum - (unsigned long)arr[lowestIdx];
+
+    printf("%ld %ld", lowestPossible, highestPossible);
+}
